16598f331f2ba376c8db84ed96c3643d580efff1
Sanjupriya Manikyala <sanjupriya.manikyala@cesltd.com>
2023-07-15 20:25:48 +0530

    redux tests

---
+++ src/actions/actions.test.js
@@ -0,0 +1,14 @@
+// actions.test.js
+import { incrementCounter, decrementCounter } from './actions';
+
+describe('actions', () => {
+  it('should create an action to increment the counter', () => {
+    const expectedAction = { type: 'INCREMENT_COUNTER' };
+    expect(incrementCounter()).toEqual(expectedAction);
+  });
+
+  it('should create an action to decrement the counter', () => {
+    const expectedAction = { type: 'DECREMENT_COUNTER' };
+    expect(decrementCounter()).toEqual(expectedAction);
+  });
+});
